3d10a2a00052ba9d0de06e77e72c90b5307b9081
dan kiss and Jared Clemmensen <dan@>
2018-03-21 15:34:18 -0700

    successfully implement api call and display stolen bike titles to page

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import $ from 'jquery';
 import { Stolen } from './stolen'
 
+var displayData = function(results) {
+  $(".bikeList").text('');
+  results.bikes.forEach(function(bike) {
+    $(".bikeList").append('<li>' + bike.title + '</li>');
+  });
+}
+
 $(document).ready(function() {
   $('#formLocation').submit(function(event) {
     event.preventDefault();
     let city = $('#city').val();
+    console.log(city)
     let stolen = new Stolen(city);
-    let response = stolen.makeRequest()
-    console.log(response);
+    stolen.makeRequest(displayData)
+
+    // $.get(stolen.makeRequest()).then(function(response) {
+    //   console.log(stolen,22)
+    //   debugger;
+    //   console.log(stolen['response'],33);
+    // }).fail(function(error) {
+    //   console.log('failed error');
+    // });
   });
 });
--- src/stolen.js
+++ src/stolen.js
@@ -1,30 +1,41 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 
 export class Stolen {
   constructor(city) {
     this.city = city;
+    // this.response = '';
   }
 
-  makeRequest() {
-    let request = new XMLHttpRequest();
-    let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=5&location=seattle&distance=25&stolenness=stolen&appid=f45e92290a73a41883482ba2cd230e55a258f5ec1e629825dc88f7c7ac63cd20&secret=2e7ef95d12847833adb4fdfab1464760bdb9b509ad17e4558294aaa8ed162263`;
-    let response;
-    request.onreadystatechange = function() {
-      // console.log(this.status,'need 200');
-      if (this.readyState === 4 && this.status === 200) {
-        response = JSON.parse(this.responseText);
-        console.log('good')
-      }
-      else {
-        response = 5;
-      }
-    }
+  makeRequest(displayData) {
+     let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=5&location=${this.city}&distance=25&stolenness=proximity&appid=f45e92290a73a41883482ba2cd230e55a258f5ec1e629825dc88f7c7ac63cd20&secret=2e7ef95d12847833adb4fdfab1464760bdb9b509ad17e4558294aaa8ed162263`;
+    $.get(url).then(function(results) {
+      displayData(results);
+    }).fail(function() {
+      console.log('error');
+    });
 
-    request.open("GET", url, true);
-    request.send();
+    // let that = this;
+    // let promise = new Promise(function(resolve, reject) {
+    //   let request = new XMLHttpRequest();
+    //   let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=5&location=seattle&distance=25&stolenness=proximity&appid=f45e92290a73a41883482ba2cd230e55a258f5ec1e629825dc88f7c7ac63cd20&secret=2e7ef95d12847833adb4fdfab1464760bdb9b509ad17e4558294aaa8ed162263`;
+    //   request.onload = function() {
+    //     if (this.status === 200) {
+    //       resolve(request.response);
+    //     }
+    //     else {
+    //       reject(Error(request.statusText));
+    //     }
+    //   }
+    //   request.open("GET", url, true);
+    //   request.send();
+    // });
+    //
+    // promise.then(function(response) {
+    //   that.response = JSON.parse(response);
+    // }, function(error) {
+    //   that.response = `There was an error processing your request: ${error.message}`;
+    // });
+    // return this.response;
 
-    this.response = response;
-    // console.log(response);
-    return response;
   }
 }
