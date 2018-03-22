faf5e74268aa3d31270347a50911b4fc92c37eac
dan kiss and Jared Clemmensen <jared@>
2018-03-22 16:18:45 -0700

    add way to see stolen bikes by zipcode with google maps api

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import $ from 'jquery';
 import { Stolen } from './stolen'
-
+export let stolen;
 var displayData = function(results) {
   if (results.bikes.length == 0) {
     $(".bikeList").text('No matches');
   } else {
     $(".bikeList").text('15 most recently stolen bikes matching your search:')
     results.bikes.forEach(function(bike) {
-      $(".bikeList").append('<li>' + bike.title + ', ' + bike.stolen_location + ', ' + bike.frame_colors +', ' + bike.manufacturer_name +', ' + bike.serial +'</li>');
+      $(".bikeList").append('<li>' + bike.title + ', ' + '<span class="location">' + bike.stolen_location + '</span>' + ', ' + bike.frame_colors +', ' + bike.manufacturer_name +', ' + bike.serial +'</li>');
       });
     }
   }
@@ -28,7 +28,6 @@ $(document).ready(function() {
     let city = $('#city').val();
     let color = $('#color').val();
     let manufacturer = $('#manufacturer').val();
-    // let serial = $('#serial').val();
     if (city.length > 0) {
       parameters.city = city;
     }
@@ -38,19 +37,25 @@ $(document).ready(function() {
     if (manufacturer.length > 0) {
       parameters.manufacturer = manufacturer;
     }
-    // if (serial.length > 0) {
-    //     parameters.serial = serial;
-    // }
-    let stolen = new Stolen(parameters);
+
+    stolen = new Stolen(parameters);
     stolen.makeRequest(displayData,displaySearch,errorMessageUser);
 
-    // $.get(stolen.makeRequest()).then(function(response) {
-    //   console.log(stolen,22)
-    //   debugger;
-    //   console.log(stolen['response'],33);
-    // }).fail(function(error) {
-    //   console.log('failed error');
-    // });
+    let locations = document.getElementsByClassName('location');
+    let locationsIsolated = [];
+    Array.from(locations).forEach(function(item) {
+      locationsIsolated.push(item.innerHTML);
+    });
+    // locations = locations.map(x => x.innerHtml);
+    setTimeout(function(){ console.log(stolen.single,'yep');}, 5000);
+    // console.log(stolen.single,'yep');
+
+    console.log(locationsIsolated);
+
+    $('#mapButton').click(function(event) {
+      stolen.getLatLong();
+    })
+
   });
 
 });
--- src/stolen.js
+++ src/stolen.js
@@ -5,44 +5,38 @@ export class Stolen {
     this.city = parameters.city;
     this.color = parameters.color;
     this.manufacturer = parameters.manufacturer;
-    // this.serial = parameters.serial;
+    this.single = [];
   }
 
   makeRequest(displayData,displaySearch,errorMessageUser) {
+    let that = this;
+    let zipCodeArray2 = [];
     console.log('city:', this.city, 'color:', this.color, 'man', this.manufacturer);
     displaySearch()
-     let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=15&location=${this.city}&distance=25&stolenness=proximity&appid=f45e92290a73a41883482ba2cd230e55a258f5ec1e629825dc88f7c7ac63cd20&secret=2e7ef95d12847833adb4fdfab1464760bdb9b509ad17e4558294aaa8ed162263&query=${this.color}&manufacturer=${this.manufacturer}`;
+     let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=15&location=${this.city}&distance=25&stolenness=proximity&appid=${process.env.BIKE_API_KEY}&secret=${process.env.BIKE_SECRET}&query=${this.color}&manufacturer=${this.manufacturer}`;
     $.get(url).then(function(results) {
+
+      results.bikes.forEach(function(bike){
+        let single = bike.stolen_location.match(/\d+/g);
+        zipCodeArray2.push(single[0]);
+        window.zipCodeArray.push(single[0]);
+      });
+      that.single = zipCodeArray2
       displayData(results);
     }).fail(function() {
       errorMessageUser()
     });
-
-
-
-
-    // let that = this;
-    // let promise = new Promise(function(resolve, reject) {
-    //   let request = new XMLHttpRequest();
-    //   let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=5&location=seattle&distance=25&stolenness=proximity&appid=f45e92290a73a41883482ba2cd230e55a258f5ec1e629825dc88f7c7ac63cd20&secret=2e7ef95d12847833adb4fdfab1464760bdb9b509ad17e4558294aaa8ed162263`;
-    //   request.onload = function() {
-    //     if (this.status === 200) {
-    //       resolve(request.response);
-    //     }
-    //     else {
-    //       reject(Error(request.statusText));
-    //     }
-    //   }
-    //   request.open("GET", url, true);
-    //   request.send();
-    // });
-    //
-    // promise.then(function(response) {
-    //   that.response = JSON.parse(response);
-    // }, function(error) {
-    //   that.response = `There was an error processing your request: ${error.message}`;
-    // });
-    // return this.response;
-
   }
+
+  getLatLong() {
+    window.zipCodeArray.forEach(function(zip) {
+      let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${zip}`;
+      $.get(url).then(function(results) {
+        window.latLongArray.push(results.results[0].geometry.location);
+      }).fail(function() {
+        console.log('error in getLatLong');
+      });
+    });
+  }
+
 }
